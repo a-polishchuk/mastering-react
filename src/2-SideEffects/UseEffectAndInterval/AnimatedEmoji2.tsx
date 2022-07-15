dee5025628b56b18aea26c7d8c68b8f60d573b9d
Andrii Polishchuk <polishchuk91@gmail.com>
2022-07-15 18:16:58 +0300

    + second iteration for UseGlobalEvents and UseEffectAndInterval

---
+++ src/2-SideEffects/UseEffectAndInterval/AnimatedEmoji2.tsx
@@ -0,0 +1,21 @@
+import { useInterval } from 'hooks/useInterval';
+import { useState } from 'react';
+
+interface Props {
+  emojis: string[];
+  delay: number;
+  fontSize: number;
+}
+
+export function AnimatedEmoji({ emojis, delay, fontSize }: Props): JSX.Element {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useInterval(() => {
+    setCurrentIndex((value) => {
+      const newValue = value + 1;
+      return newValue >= emojis.length ? 0 : newValue;
+    });
+  }, delay);
+
+  return <div style={{ fontSize }}>{emojis[currentIndex]}</div>;
+}
---
+++ src/2-SideEffects/UseGlobalEvents/UseGlobalEvents2.tsx
@@ -0,0 +1,33 @@
+import { ChapterHeader } from 'components/ChapterHeader';
+import { Toolbar } from 'components/Toolbar';
+import { useState } from 'react';
+
+import { useKeyDown } from './useKeyDown';
+import { WindowSizePreview } from './WindowSizePreview';
+
+const STEP = 10;
+const MIN_WIDTH = 100;
+const MAX_WIDTH = 300;
+
+export function UseGlobalEvents(): JSX.Element {
+  const [previewWidth, setPreviewWidth] = useState<number>((MAX_WIDTH + MIN_WIDTH) / 2);
+
+  useKeyDown((buttonKey: string) => {
+    switch (buttonKey) {
+      case 'ArrowLeft':
+        setPreviewWidth((value) => Math.max(value - STEP, MIN_WIDTH));
+        break;
+      case 'ArrowRight':
+        setPreviewWidth((value) => Math.min(value + STEP, MAX_WIDTH));
+        break;
+    }
+  });
+
+  return (
+    <>
+      <ChapterHeader title="useEffect" subtitle="Listening to global events" />
+      <Toolbar>Use ⬅️ ➡️ to change the preview window size.</Toolbar>
+      <WindowSizePreview previewWidth={previewWidth} />
+    </>
+  );
+}
