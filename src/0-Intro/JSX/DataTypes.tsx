87db9a31a215940fc7f15ac9d3af5ed8dd0f4978
Andrii Polishchuk <polishchuk91@gmail.com>
2022-09-18 18:41:45 +0300

    ~ Polishing DataTypes chapter

--- src/0-Intro/JSX/DataTypes.tsx
+++ src/0-Intro/JSX/DataTypes.tsx
@@ -7,30 +7,28 @@ export function DataTypes(): JSX.Element {
     <ChapterWrapper title="Data types" subtitle="Introduction to JSX">
       <h3>Let's check how React renders different data types</h3>
       <br />
-      <div style={{ display: 'flex', gap: 24 }}>
-        <table>
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>Value</th>
-              <th>Result</th>
+      <table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Value</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderValues.map(({ type, valueString, value }, index) => (
+            <tr key={index}>
+              <td>
+                <b>{type}</b>
+              </td>
+              <td>
+                <code>{valueString}</code>
+              </td>
+              <td>{value}</td>
             </tr>
-          </thead>
-          <tbody>
-            {renderValues.map(({ type, valueString, value }, index) => (
-              <tr key={index}>
-                <td>
-                  <b>{type}</b>
-                </td>
-                <td>
-                  <code>{valueString}</code>
-                </td>
-                <td>{value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </ChapterWrapper>
   );
 }
