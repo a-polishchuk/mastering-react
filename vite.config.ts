101b7485c9a6102336c2c85269975d52e1c12b68
Andrii Polishchuk <polishchuk91@gmail.com>
2022-09-19 12:26:25 +0300

    ~ Upgraded some dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// TODO: try to use triple slash to configure vitest types
 export default defineConfig({
   base: '/mastering-react/',
   plugins: [react() as any, tsconfigPaths()],
