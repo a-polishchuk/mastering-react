10ac25a2852bcffa3a5e0f102275bb46560b53bc
Andrii Polishchuk <polishchuk91@gmail.com>
2022-10-02 12:26:28 +0300

    ~ Polishing State Management section

--- src/7-StateManagement/model/empty.ts
+++ src/7-StateManagement/model/empty.ts
@@ -1,9 +1,9 @@
-import { GridState } from '7-StateManagement/types';
+import { GridState, CellState } from '7-StateManagement/types';
 
 export function empty(rows: number, cols: number): GridState {
   const grid: GridState = new Array<number[]>(rows);
   for (let r = 0; r < rows; r++) {
-    grid[r] = new Array<number>(cols).fill(0);
+    grid[r] = new Array<number>(cols).fill(CellState.DEAD);
   }
   return grid;
 }
