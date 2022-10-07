dba4a73be0933872024084a8b0092a5e16055dad
Andrii Polishchuk <polishchuk91@gmail.com>
2022-10-07 17:41:20 +0300

    ~ Updated Hooks factory chapter

---
+++ src/9-DesignPatters/CompoundComponent/CompoundComponent.tsx
@@ -0,0 +1,5 @@
+import { ChapterWrapper } from 'components';
+
+export function CompoundComponent(): JSX.Element {
+  return <ChapterWrapper title="Compount component" subtitle="Design patterns"></ChapterWrapper>;
+}
--- src/9-DesignPatters/HooksFactory/HooksFactory.tsx
+++ src/9-DesignPatters/HooksFactory/HooksFactory.tsx
@@ -1,31 +1,37 @@
 import { ChapterWrapper } from 'components';
+import { useEffect, useState } from 'react';
 
 import { createMappedState } from './createMappedState';
 import { Field } from './Field';
 
-const useLowercaseState = createMappedState((newValue) => {
-  return newValue?.toLowerCase();
-});
+const useLowercaseState = createMappedState((newValue) => newValue.toLowerCase());
+const useUppercaseState = createMappedState((newValue) => newValue.toUpperCase());
+const useNoSpacesState = createMappedState((newValue) => newValue.replaceAll(/\s/g, ''));
+const useNoNumbersState = createMappedState((newValue) => newValue.replaceAll(/[0-9]/g, ''));
 
-const useUppercaseState = createMappedState((newValue) => {
-  return newValue?.toUpperCase();
-});
-
-const useNoSpacesState = createMappedState((newValue) => {
-  return newValue?.replaceAll(/\s/g, '');
-});
-
-// TODO: add one more input with the original text
 export function HooksFactory(): JSX.Element {
-  const [lowercase, setLowercase] = useLowercaseState('SOME VALUE');
-  const [uppercase, setUppercase] = useUppercaseState('some value');
-  const [noSpaces, setNoSpaces] = useNoSpacesState('  SOME VALUE    ');
+  const [originalValue, setOriginalValue] = useState<string>('Some Value 123');
+
+  const [lowercase, setLowercase] = useLowercaseState('');
+  const [uppercase, setUppercase] = useUppercaseState('');
+  const [noSpaces, setNoSpaces] = useNoSpacesState('');
+  const [noNumbers, setNoNumbers] = useNoNumbersState('');
+
+  useEffect(() => {
+    setLowercase(originalValue);
+    setUppercase(originalValue);
+    setNoSpaces(originalValue);
+    setNoNumbers(originalValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [originalValue]);
 
   return (
-    <ChapterWrapper title="Hooks Factory" subtitle="Design patterns">
+    <ChapterWrapper title="Hooks factory" subtitle="Design patterns">
+      <Field label="Regular input" value={originalValue} onChange={setOriginalValue} />
       <Field label="lowercase only" value={lowercase} onChange={setLowercase} />
       <Field label="UPPERCASE ONLY" value={uppercase} onChange={setUppercase} />
-      <Field label="NoSpacesHere" value={noSpaces} onChange={setNoSpaces} />
+      <Field label="NoSpaces" value={noSpaces} onChange={setNoSpaces} />
+      <Field label="No numbers" value={noNumbers} onChange={setNoNumbers} />
     </ChapterWrapper>
   );
 }
