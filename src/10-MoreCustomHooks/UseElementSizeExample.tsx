b912eb68f6d2e0cc210b7bead862b86fdbfe9a7e
Andrii Polishchuk <polishchuk91@gmail.com>
2022-10-14 15:58:01 +0300

    + useElementSize chapter

--- src/10-MoreCustomHooks/UseElementSizeExample.tsx
+++ src/10-MoreCustomHooks/UseElementSizeExample.tsx
@@ -1,6 +1,7 @@
-import { ChapterWrapper } from 'components';
+import classNames from 'classnames';
+import { Button, ChapterWrapper, Toolbar } from 'components';
 import { useElementSize } from 'hooks/useElementSize';
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import { generateRandomColor } from 'utils/generateRandomColor';
 
 import classes from './UseElementSize.module.css';
@@ -8,17 +9,36 @@ import classes from './UseElementSize.module.css';
 export function UseElementSizeExample(): JSX.Element {
   const [size, refCallback] = useElementSize();
   const [width, height] = size;
+  const [dummiesCount, setDummiesCount] = useState<number>(0);
+
+  const addDummy = () => setDummiesCount((val) => val + 1);
+  const removeDummy = () => setDummiesCount((val) => Math.max(val - 1, 0));
 
   const targetStyle: CSSProperties = {
     backgroundColor: generateRandomColor(),
   };
 
+  const dummies = new Array<JSX.Element>(dummiesCount);
+  for (let i = 0; i < dummiesCount; i++) {
+    dummies[i] = (
+      <div key={i} className={classNames(classes.node, classes.dummy)}>
+        Dummy
+      </div>
+    );
+  }
+
   return (
     <ChapterWrapper title="useElementSize" subtitle="More custom hooks">
+      <Toolbar>
+        <Button text="Add dummy" onClick={addDummy} />
+        <Button text="Remove dummy" onClick={removeDummy} />
+      </Toolbar>
+
       <div className={classes.container}>
-        <div ref={refCallback} className={classes.target} style={targetStyle}>
+        <div ref={refCallback} className={classes.node} style={targetStyle}>
           {width.toFixed(0)} x {height.toFixed(0)}
         </div>
+        {dummies}
       </div>
     </ChapterWrapper>
   );
--- src/hooks/useElementSize.ts
+++ src/hooks/useElementSize.ts
@@ -1,17 +1,30 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useRef, useEffect } from 'react';
 import { RefCallback } from 'types/RefCallback';
 
 export type Size = [number, number];
+
 const INITIAL_SIZE: Size = [0, 0];
 
 export function useElementSize(): [Size, RefCallback] {
   const [size, setSize] = useState<Size>(INITIAL_SIZE);
+  const observerRef = useRef<ResizeObserver>();
+
+  if (!observerRef.current) {
+    observerRef.current = new ResizeObserver((entries) => {
+      const { inlineSize, blockSize } = entries[0].borderBoxSize[0];
+      setSize([inlineSize, blockSize]);
+    });
+  }
 
   const refCallback = useCallback((element: HTMLElement | null) => {
+    observerRef.current?.disconnect();
     if (element) {
-      const { width, height } = element.getBoundingClientRect();
-      setSize([width, height]);
+      observerRef.current?.observe(element);
     }
+  }, []);
+
+  useEffect(() => {
+    return () => observerRef.current?.disconnect();
   }, []);
 
   return [size, refCallback];
