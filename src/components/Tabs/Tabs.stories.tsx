9eeff09d7f637a73d91dc38965d92dd06d6df162
Andrii Polishchuk <polishchuk91@gmail.com>
2022-10-28 11:55:12 +0300

    + Tabs with Switch story

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { useState } from 'react';
 
+import { Case, Switch } from '../Switch/Switch';
 import { TextBlock } from '../TextBlock/TextBlock';
 import { Tabs, TabsProps } from './Tabs';
 
@@ -31,5 +32,32 @@ export const Default: ComponentStory<typeof Tabs> = (args: TabsProps) => {
 
 Default.storyName = 'Tabs';
 Default.args = {
+  tabs: TABS,
+};
+
+export const TabsWithSwitch: ComponentStory<typeof Tabs> = (args: TabsProps) => {
+  const [selectedTab, setSelectedTab] = useState<string>(TABS[0]);
+
+  return (
+    <>
+      <Tabs {...args} selectedTab={selectedTab} onSelectedTabChange={setSelectedTab} />
+      <div style={{ marginTop: 10 }}>
+        <Switch value={selectedTab}>
+          <Case value={TABS[0]}>
+            <TextBlock>1️⃣ First tab content</TextBlock>
+          </Case>
+          <Case value={TABS[1]}>
+            <TextBlock>2️⃣ Second tab content</TextBlock>
+          </Case>
+          <Case value={TABS[2]}>
+            <TextBlock>3️⃣ Third tab content</TextBlock>
+          </Case>
+        </Switch>
+      </div>
+    </>
+  );
+};
+
+TabsWithSwitch.args = {
   tabs: TABS,
 };
