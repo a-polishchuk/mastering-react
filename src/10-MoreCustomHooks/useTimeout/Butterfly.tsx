57171a8e480ba0c4858d57c1ed64c48e520cf0e2
Andrii Polishchuk <polishchuk91@gmail.com>
2025-01-09 15:49:28 +0200

    ~ improved useInterval/useTimeout example

--- src/10-MoreCustomHooks/useTimeout/Butterfly.tsx
+++ src/10-MoreCustomHooks/useTimeout/Butterfly.tsx
@@ -1,7 +1,7 @@
 import { useInterval } from 'hooks/useInterval';
-import { useToggle } from 'hooks/useToggle';
 import { CSSProperties, useEffect, useState } from 'react';
 import { Position } from 'utils/geometry';
+import classes from './Butterfly.module.css';
 import { Creature } from './Creature';
 import { generateRandomPosition } from './generateRandomPosition';
 
@@ -11,23 +11,22 @@ type Props = {
 
 export function Butterfly({ creature }: Props) {
     const [position, setPosition] = useState<Position>(creature.position);
-    const [toggled, toggle] = useToggle(false);
-    const angle = toggled ? 20 : -20;
 
     useEffect(() => setPosition(generateRandomPosition()), []);
 
     useInterval(() => setPosition(generateRandomPosition()), 5000);
 
-    useInterval(toggle, 100);
-
     const style: CSSProperties = {
         position: 'absolute',
         left: `${position.x}%`,
         top: `${position.y}%`,
-        transform: `translate(-50%, -50%) rotate(${angle}deg)`,
         cursor: 'default',
         transition: 'transform 0.1s ease-in-out, left 5s linear, top 5s linear',
     };
 
-    return <div style={style}>🦋</div>;
+    return (
+        <div className={classes.wingFlutter} style={style}>
+            🦋
+        </div>
+    );
 }
