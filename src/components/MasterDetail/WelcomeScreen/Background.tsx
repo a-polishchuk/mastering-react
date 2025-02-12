fe7fab80aca34bda13ef7f43d8ab0ed971dee268
Andrii Polishchuk <polishchuk91@gmail.com>
2025-02-12 10:02:03 +0200

    + particles background

--- src/components/MasterDetail/WelcomeScreen/Background.tsx
+++ src/components/MasterDetail/WelcomeScreen/Background.tsx
@@ -1,19 +1,77 @@
-import { ReactLogo } from 'assets/ReactLogo';
-import { useElementSize } from 'hooks/useElementSize';
-import { useMemo } from 'react';
-import { generateRandomColor } from 'utils/generateRandomColor';
+import Particles from 'react-tsparticles';
 import classes from './Background.module.css';
 
 export function Background() {
-    const color = useMemo(() => generateRandomColor() + '33', []);
-    const [size, refCallback] = useElementSize();
-    const [width] = size;
-
     return (
-        <div ref={refCallback} className={classes.container}>
-            <div className={classes.logoContainer}>
-                <ReactLogo size={width} color={color} className={classes.logo} />
-            </div>
+        <div className={classes.container}>
+            <Particles
+                id="tsparticles"
+                options={{
+                    fullScreen: false,
+                    background: {
+                        color: {
+                            value: 'transparent',
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: 'push',
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: 'repulse',
+                            },
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: '#000000',
+                        },
+                        links: {
+                            color: '#000000',
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 0.5,
+                        },
+                        move: {
+                            direction: 'none',
+                            enable: true,
+                            outModes: {
+                                default: 'bounce',
+                            },
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            value: 100,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: 'circle',
+                        },
+                        size: {
+                            value: { min: 1, max: 2 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
         </div>
     );
 }
