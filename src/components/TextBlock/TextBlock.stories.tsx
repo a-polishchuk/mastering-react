e96fb84b006abc8edc3010c1d5f185068e81fd20
Andrii Polishchuk <polishchuk91@gmail.com>
2025-04-21 19:39:14 +0300

    + different TextBlock types

--- src/components/TextBlock/TextBlock.stories.tsx
+++ src/components/TextBlock/TextBlock.stories.tsx
@@ -1,15 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TextBlock } from './TextBlock';
 
-const TEXT = (
-    `
+const TEXT = `
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor 
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
         nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
         eu fugiat nulla pariatur.
-    `
-);
+    `;
 
 export default {
     component: TextBlock,
